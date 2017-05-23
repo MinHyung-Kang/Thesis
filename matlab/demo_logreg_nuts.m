ad363a7c1499baecfa35e943e4eb0466a92db136
Daniel Kang <minhyung.daniel.kang@gmail.com>
2017-05-23 01:30:53 -0400

    Hopefully

--- matlab/demo_logreg_nuts.m
+++ matlab/demo_logreg_nuts.m
@@ -29,13 +29,13 @@ timeStepUnit = maxIter / numTimeSteps;
 adverIter = 10;
 optNum = length(MOpts);
 algNames = {'SVGD','Random Subset', 'Random Subset + Control Functional', ...
-%    'Induced Points'};
     'Induced Points', 'Adversarial Induced Points (10 updates)'};
+%    'Induced Points'};
 numModels = length(algNames);
 baseModel = getModel('none',-1);                                   % Base Model
 
 %% NUTS Sampling
-for datasetInd = 1:length(benchmarks)
+for datasetInd = 7:length(benchmarks)
     dataset = benchmarks{datasetInd};
     % If saved, load that dataset
     datasetName = sprintf('%s_%d_%d.mat', dataset,n_warm_up,N);
@@ -108,21 +108,21 @@ for datasetInd = 1:length(benchmarks)
 
             % Evaluate for each model
             for modelInd = 1:length(modelOpts)
-                fprintf('Evaluating : (M=%d) Trial (%d/%d) Model : %s \n',...
-                    M, trialInd, maxTrial, algNames{modelInd});
+                fprintf('[%s]Evaluating : (M=%d) Trial (%d/%d) Model : %s \n',...
+                    dataset, M, trialInd, maxTrial, algNames{modelInd});
 
                 modelOpt = modelOpts{modelInd};
                 modelOpt.baseSeed = trialInd * maxIter;
-                
+
                 % Get update
                 timeStart = tic;
-                
+
 %                  for iter = 1:maxIter
 %                     % Get update
 %                     timeStart = tic;
 %                     modelOpt.nextSeed = trialInd * maxIter + iter;
 %                     [theta, gradInfo, modelOpt] = svgd_singleIteration(theta, dlog_p, gradInfo, modelOpt);
-%                  end                 
+%                  end
 
                 modelOpt.baseSeed = trialInd * maxIter;
                 theta = svgd(theta0, dlog_p, maxIter, modelOpt);
@@ -130,7 +130,7 @@ for datasetInd = 1:length(benchmarks)
 
                 [MSE_x, MSE_cov] = evalPerformance(theta);
                 HInfo= evalMMD(theta);
-                
+
 
                 mse_x(modelInd, mInd) = mse_x(modelInd, mInd) + MSE_x / maxTrial;
                 mse_cov(modelInd, mInd) = mse_cov(modelInd, mInd) + MSE_cov / maxTrial;
@@ -140,31 +140,37 @@ for datasetInd = 1:length(benchmarks)
         end
 
     end
-
-    return;  % EARLY STOP HERE
+    results = {t_vals, mse_x, mse_cov, mmd_stat};
+    matName = sprintf('logreg_nuts_results_%s',datasetName);
+    save(matName,'results');    
 end
 
-%% Plot the results
-results = {t_vals, mse_x, mse_cov, mmd_stat};
-matName = sprintf('logreg_nuts_results_%s',datasetName);  
-save(matName,'results');
-figure;
+
+return;  % EARLY STOP HERE
+%% Plot the results (only mmd and time)
+figure('Position', [100, 100, 800, 250]);
+%figure;
+optNum = 9;
+algNames = {'SVGD','Random Subset', 'Random Subset + Control Functional', ...
+    'Induced Points', 'Adversarial Induced Points (10 updates)'};
+numModels = length(algNames);
 colOpts = {'h-','o-','*-','.-','x-','s-','d-','^-','v-','p-','h-','>-','<-'};
-titleNames = {'Total Time', 'Estimating mean', 'Estimating covariance', 'Maximum Mean Discrepancy'};
-yLabels = {'log10 t', 'log10 MSE', 'log10 MSE', 'test statistic'};
+titleNames = {'Total Time','Maximum Mean Discrepancy'};
+yLabels = {'log10 t', 'test statistic'};
+datasetName = 'German'
 
 MOptsTxt = {'10','20','30','50','80','100','150','200','250'};
 
-
-for j = 1:4
-    if j >= 3
-        subplot(2,3,j+1);
-    else
-        subplot(2,3,j);
-    end
-    
+for j=1:2
+    subplot(1,2,j);
     handles = zeros(1, numModels);
-    result = results{j};
+    if j == 2
+        resultInd = 4;
+    else
+        resultInd = 1;
+    end
+    result = results{resultInd};
+    
     for i = 1:numModels
         if j == 1
             handles(i) = semilogy(1:optNum, result(i,:),colOpts{i});
@@ -173,34 +179,84 @@ for j = 1:4
         end
         hold on;
     end
-    title(sprintf('%s',titleNames{j}));
-    xlabel('Sample Size (N)');
-    ylabel(yLabels{j});
+    %title(sprintf('%s',titleNames{j}),'FontSize',14);
+    xlabel('Sample Size (N)','FontSize',14);
+    ylabel(yLabels{j},'FontSize',14);
     set(gca,'Xtick',[1 4 9]);
     set(gca,'XtickLabel',{'10','50','250'});
 end
 
-subplot(2,3,6);
-axis off;
-leg1 = legend(handles, algNames, 'Orientation','vertical');
-%set(leg1, 'Position',[0.7 0.3 0 0]);
-set(leg1, 'Position',[0.8 0.5 0.05 0.05]);
+
+%subplot(1,2,3);
+%axis off;
+% leg1 = legend(handles, algNames, 'Orientation','horizontal');
+% %set(leg1, 'Position',[0.7 0.3 0 0]);
+% set(leg1, 'Position',[0.4 0.1 0.05 0.05]);
+
+ax1 = axes('Position',[0 0 1 1],'Visible','off');
+axes(ax1) % sets ax1 to current axes
+fileName = text(.025,0.38,datasetName);
+fileName.FontSize = 18;
+set(fileName, 'rotation', 90)
+
+
+
+% %% Plot the results (all)
+% figure;
+% optNum = 9;
+% algNames = {'SVGD','Random Subset', 'Random Subset + Control Functional', ...
+%     'Induced Points', 'Adversarial Induced Points (10 updates)'};
+% numModels = length(algNames);
+% colOpts = {'h-','o-','*-','.-','x-','s-','d-','^-','v-','p-','h-','>-','<-'};
+% titleNames = {'Total Time', 'Estimating mean', 'Estimating covariance', 'Maximum Mean Discrepancy'};
+% yLabels = {'log10 t', 'log10 MSE', 'log10 MSE', 'test statistic'};
+% 
+% MOptsTxt = {'10','20','30','50','80','100','150','200','250'};
+% 
+% 
+% for j = 1:4
+%     if j >= 3
+%         subplot(2,3,j+1);
+%     else
+%         subplot(2,3,j);
+%     end
+% 
+%     handles = zeros(1, numModels);
+%     result = results{j};
+%     for i = 1:numModels
+%         if j == 1
+%             handles(i) = semilogy(1:optNum, result(i,:),colOpts{i});
+%         else
+%             handles(i) = plot(1:optNum, result(i,:),colOpts{i});
+%         end
+%         hold on;
+%     end
+%     title(sprintf('%s',titleNames{j}));
+%     xlabel('Sample Size (N)');
+%     ylabel(yLabels{j});
+%     set(gca,'Xtick',[1 4 9]);
+%     set(gca,'XtickLabel',{'10','50','250'});
+% end
+% 
+% subplot(2,3,6);
+% axis off;
+% leg1 = legend(handles, algNames, 'Orientation','vertical');
+% %set(leg1, 'Position',[0.7 0.3 0 0]);
+% set(leg1, 'Position',[0.8 0.5 0.05 0.05]);
 
 %% Auxiliary functions
-function [MSE_x, MSE_xsq] = evalPoints(points, x_nuts, cov_nuts)
-    x_hat = mean(points,1);
-    cov_hat = cov(points);
-
-    MSE_x = log10(MSE(x_hat, x_nuts));
-    MSE_xsq = log10(MSE(cov_hat, cov_nuts));
-end
-
-function val = MSE(y,yhat)
-    val = mean((y(:) - yhat(:)).^2);
-end
-
-function [HInfo]= evalPointsMMD(points, points_nuts)
-    points_total = [points;points_nuts];
-    labels = [ones(1,size(points_nuts,1)) ones(1,size(points,1)) * -1];
-    [~,HInfo]= mmd(points_total, labels);
-end
+% function [MSE_x, MSE_xsq] = evalPoints(points, x_nuts, cov_nuts)
+%     x_hat = mean(points,1);
+%     cov_hat = cov(points);
+%
+%     MSE = @(y,yhat)(mean((y(:) - yhat(:)).^2));
+%
+%     MSE_x = log10(MSE(x_hat, x_nuts));
+%     MSE_xsq = log10(MSE(cov_hat, cov_nuts));
+% end
+%
+% function [HInfo]= evalPointsMMD(points, points_nuts)
+%     points_total = [points;points_nuts];
+%     labels = [ones(1,size(points_nuts,1)) ones(1,size(points,1)) * -1];
+%     [~,HInfo]= mmd(points_total, labels);
+% end
